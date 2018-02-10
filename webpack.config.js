825e02b0f6d60fed1e9a9d6b90f4841ec2535e0b
Lukas <lukas_sturm@yahoo.de>
2018-02-10 15:48:05 +0100

    :sparkles: adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/controller.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
