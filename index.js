6eef01bcc1ceb296ca9c691e063c156eaac6c12c
Lukas <lukas_sturm@yahoo.de>
2018-02-23 00:23:40 +0100

    :rocket: :art:

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 let colors = ["#3444FF", "#FF2B25", "#B616E8", "#E8964C", "#FFE46A"];
 
 this.polynetz = new Polynetz({
-	background_color: "#222",
-	connection_color: "255,99,71",
+	background_color: "linear-gradient(to bottom right, lime, white)",
+	background_is_gradient: true,
+	connection_color: "255,255,255", // 255,99,71
 	slices: 10,
 	connection_radius: 100,
 	max_connection_radius: 150,
 	connection_mode: "freeze_under_mouse",
 	connection_is_transparent: true,
 	render_poly: false,
-	freeze_multiplier: 100,
+	freeze_multiplier: 1,
 	mouse: {
 		push_radius: 100,
 		freeze_radius: 100,
@@ -33,33 +34,29 @@ this.polynetz = new Polynetz({
 });
 
 window.onload = () => {
-	init();
+  if (!this.polynetz.init()) return;
+
+  this.polynetz.addEventlistener("click", evt => {
+    this.polynetz.addPoly({
+      x: this.polynetz.mouse_location.x,
+      y: this.polynetz.mouse_location.y,
+    }, {
+      color: "rgba(255,255,255,0.65)",
+      size_radius: 2,
+      bounce: false,
+    });
+  });
+
+  this.polynetz.addEventlistener("reset", evt => {
+    console.log(`Cleared: ${evt.amount}`);
+    spawnBulk(100, {x: 200, y: 300}, true, false);
+  });
+
+  spawnBulk(200);
+  this.polynetz.start();
+
+  console.log(this.polynetz.getLoadedConnectionFunctions());
 };
-
-function init() {
-	this.polynetz.init();
-
-	this.polynetz.addEventlistener("click", evt => {
-		this.polynetz.addPoly({
-			x: this.polynetz.mouse_location.x,
-			y: this.polynetz.mouse_location.y,
-		}, {
-			color: "rgba(255,255,255,0.65)",
-			size_radius: 2,
-			bounce: false,
-		});
-	});
-
-	this.polynetz.addEventlistener("reset", evt => {
-		console.log(`Cleared: ${evt.amount}`);
-		spawnBulk(100, {x: 200, y: 300}, true, false);
-	});
-
-	spawnBulk(200);
-	this.polynetz.start();
-
-	console.log(this.polynetz.getLoadedConnectionFunctions());
-}
 
 let getRandomColor = () => colors[Math.round(Math.random() *4)];
 let getRandomNumber = (min, max) => Math.round(min + (Math.random() * (max - min)));
