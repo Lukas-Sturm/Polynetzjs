a160fab748185fee3596bdb8187c83ffe958d803
Lukas <lukas_sturm@yahoo.de>
2018-06-22 21:06:21 +0200

    General changes

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 let colors = ["#3444FF", "#FF2B25", "#B616E8", "#E8964C", "#FFE46A"];
 
 this.polynetz = new Polynetz({
-	background_color: "linear-gradient(to bottom right, lime, white)",
-	background_is_gradient: true,
-	connection_color: "255,255,255", // 255,99,71
+	background_color: "#222", //linear-gradient(to bottom right, lime, white)
+	//background_is_gradient: true,
+	connection_color: "#fff", // 
 	slices: 10,
 	connection_radius: 100,
 	max_connection_radius: 150,
-	connection_mode: "freeze_under_mouse",
+	connection_mode: "connect_only_mouse",
 	connection_is_transparent: true,
-	render_poly: false,
-	freeze_multiplier: 1,
+	render_poly: true,
+	freeze_multiplier: 0.5,
 	mouse: {
 		push_radius: 100,
 		freeze_radius: 100,
@@ -20,7 +20,7 @@ this.polynetz = new Polynetz({
 		color: "white",	
 	},
 	debug: {
-		grid: false,
+		grid: true,
 		connection_radius: false,
 		render_connection_grid: false,
 		render_push_radius: false,
@@ -29,7 +29,8 @@ this.polynetz = new Polynetz({
 	poly: {
 		speed_multiplikator: 3,
 		size_radius: 3,
-		color: "#ff6347",
+		color: "#fff",
+		bounce: true,
 	},
 });
 
@@ -43,7 +44,7 @@ window.onload = () => {
     }, {
       color: "rgba(255,255,255,0.65)",
       size_radius: 2,
-      bounce: false,
+      bounce: true,
     });
   });
 
@@ -52,7 +53,7 @@ window.onload = () => {
     spawnBulk(100, {x: 200, y: 300}, true, false);
   });
 
-  spawnBulk(200);
+  spawnBulk(200, true, true);
   this.polynetz.start();
 
   console.log(this.polynetz.getLoadedConnectionFunctions());
@@ -65,6 +66,6 @@ function spawnBulk(amount, randomColor=false, randomRadius=false) {
 	for (let i = 0; i < amount; i++) {
 		let radius = getRandomNumber(2, 6);
 		console.log(`Created Poly with Radius of ${radius}`);
-		this.polynetz.addPoly({x: getRandomNumber(0, this.polynetz.width), y: getRandomNumber(0, this.polynetz.height)}, { color: randomColor ? getRandomColor() : "white", radius: randomRadius ? radius : 10, bounce:true});
+		this.polynetz.addPoly({x: getRandomNumber(0, this.polynetz.width), y: getRandomNumber(0, this.polynetz.height)}, { color: randomColor ? getRandomColor() : "white", radius: randomRadius ? radius : 10, bounce: true});
 	}
 }
